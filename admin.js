c296b9960a76b6a3da00ed6ee5ba284a0124a8ce
neha-ahmed <61065291+neha-ahmed@users.noreply.github.com>
2020-03-17 00:09:42 +0500

    admin panel changes

--- admin.js
+++ admin.js
@@ -12,8 +12,36 @@ const Item = require('./models/Item');
 AdminBro.registerAdapter(AdminBroMongoose)
 
 const adminBro = new AdminBro({
-  databases: [mongoose],
-  rootPath: '/admin',
-})
+  // Passing resources one by one
+    rootPath: '/admin',
+    resources: [
+      {
+        resource: User,     
+        options: {
+            name: { isVisible: { list: true, filter: true, show: true, edit: true } },
+            email: { isVisible: { list: true, filter: true, show: true, edit: true } },
+            password: { isVisible: { list: false, filter: false, show: false, edit: true } },
+        }
+      },
+      {
+        resource: Teacher,
+        options: {
+            name: { isVisible: { list: true, filter: true, show: true, edit: true } },
+            Qualification: { isVisible: { list: true, filter: true, show: true, edit: true } },
+          }
+      },
+      {
+        resource: Item,
+        options: {
+            name: { isVisible: { list: true, filter: true, show: true, edit: true } },
+          }
+      }
+    ],
+    branding: {
+        logo: 'bulb3.png',
+        companyName: 'Professor',
+        softwareBrothers: false   // if Software Brothers logos should be shown in the sidebar footer
+      },
+  })
 
 module.exports = adminRouter = AdminBroExpress.buildRouter(adminBro)

f42794f12f9262973a6bbb7359ff0023ecb847e0
neha-ahmed <61065291+neha-ahmed@users.noreply.github.com>
2020-03-18 00:26:02 +0500

    comp

--- client/src/App.js
+++ client/src/App.js
@@ -1,113 +1,67 @@
-import React, { Component } from "react";
+import React, { Component, useState, useEffect } from "react";
 
 import axios from "axios";
 import "./App.css";
 import { BrowserRouter, Switch, Route, NavLink } from "react-router-dom";
-import LoginSystem from "./components/LoginSystem";
-import Login from "./components/Login";
-import Dashboard from "./components/Dashboard";
-import Home from "./components/Home";
-import teacherheader from "./components/TeacherPage/teacherheader";
 
+import HomeFinal from "./components/Homepage/HomeFinal";
 import App2 from "./App2";
-class App extends Component {
-  state = {
-    items: [],
-    id: 0,
-    message: null,
-    intervalIsSet: false,
-    idToDelete: null,
-    idToUpdate: null,
-    objectToUpdate: null
-  };
 
-  // never let a process live forever
-  // always kill a process everytime we are done using it
-  componentWillUnmount() {
-    if (this.state.intervalIsSet) {
-      clearInterval(this.state.intervalIsSet);
-      this.setState({ intervalIsSet: null });
-    }
-  }
+import RegisterNew from "./components/LoginSignup/RegisterNew";
+import Register from "./components/LoginSignup/Register";
+import Dashboard from "./components/Dashboard";
+import PrivateRoute from "./Utils/PrivateRoute";
+import PublicRoute from "./Utils/PublicRoute";
+import { getToken, removeUserSession, setUserSession } from "./Utils/Common";
+import LoginSystem from '../src/components/LoginSystem';
+ 
 
-  //Getting from DB
-  componentDidMount() {
-    axios.get(`/api/items/`).then(res => {
-      const items = res.data;
-      this.setState({ items });
-    });
-  }
+function App() {
+  const [authLoading, setAuthLoading] = useState(true);
 
-  //Post request, putting data to DB
-  putDataToDB = name => {
-    let currentIds = this.state.items.map(items => items.id);
-    let idToBeAdded = 0;
-    while (currentIds.includes(idToBeAdded)) {
-      ++idToBeAdded;
+  useEffect(() => {
+    const token = getToken();
+    if (!token) {
+      return;
     }
 
-    axios.post("/api/items", {
-      id: idToBeAdded,
-      name: name
-    });
-  };
+    axios
+      .get(`/verifyToken?token=${token}`)
+      .then(response => {
+        setUserSession(response.data.token, response.data.user);
+        setAuthLoading(false);
+      })
+      .catch(error => {
+        removeUserSession();
+        setAuthLoading(false);
+      });
+  }, []);
 
-  //DELETING FROM DATABASE
-  deleteFromDB = idTodelete => {
-    parseInt(idTodelete);
-    let objIdToDelete = null;
-    this.state.items.forEach(items => {
-      if (items.id === idTodelete) {
-        objIdToDelete = items._id;
-      }
-    });
+  if (authLoading && getToken()) {
+    return <div className="content">Checking Authentication...</div>;
+  }
 
-    axios.delete(`/api/items/${idTodelete}`, {
-      items: {
-        id: objIdToDelete
-      }
-    });
-  };
-
-  // our update method that uses our backend api
-  // to overwrite existing data base information
-  updateDB = (idToUpdate, updateToApply) => {
-    let objIdToUpdate = null;
-    parseInt(idToUpdate);
-    this.state.data.forEach(dat => {
-      if (dat.id === idToUpdate) {
-        objIdToUpdate = dat._id;
-      }
-    });
-
-    axios.post("api/items/updateData", {
-      id: objIdToUpdate,
-      update: { message: updateToApply }
-    });
-  };
-
-  render() {
-    const { items } = this.state;
-    return (
-      <div>
-         <teacherheader />
-        <BrowserRouter>
-          <div>
-            <div className="header">
-              <NavLink exact activeClassName="active" to="/2">
-                Home
-              </NavLink>
-            </div>
-            <div className="content">
-              <Switch>
-                <Route exact path="/2" component={App2} />
-              </Switch>
-            </div>
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+          <div className="header"></div>
+          <div className="content">
+            <Switch>
+              <Route exact path="/" component={HomeFinal} />
+              
+              <Route exact path="/2" component={App2} />
+              <Route exact path="/3" component={LoginSystem} />
+              <PublicRoute exact path="/login" component={RegisterNew} />
+              
+              <PublicRoute exact path="/Register" component={Register} />
+              <PrivateRoute path="/dashboard" component={Dashboard} />
+            </Switch>
           </div>
-        </BrowserRouter>
-      </div>
-    );
-  }
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;

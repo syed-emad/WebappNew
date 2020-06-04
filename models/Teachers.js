70bcbd96a574f6d9f168c42e78b831617144cd1f
neha-ahmed <61065291+neha-ahmed@users.noreply.github.com>
2020-06-04 21:00:30 +0500

    xx

--- models/Teachers.js
+++ models/Teachers.js
@@ -13,6 +13,36 @@ const BookingSchema = new Schema({
   BookingTime:String,
   Price:String,
 });
+
+//education schema
+
+const EducationScehma= new Schema ({
+  level: String,
+  institute: String,
+  startDate: String,
+  endDate: String,
+  field: String
+});
+//work experience schema
+
+const WorkSchema = new Schema({
+  title: String,
+  place: String,
+  startDate:String,
+  endDate: String,
+  details: String
+});
+
+//teacher's schedule schema
+
+const ScheduleSchema = new Schema ({
+  Day: String,
+  Date: String,
+  Time: String,
+  Subject: String,
+  Price:String
+});
+
 const AccountSchema =new Schema({
   classID: String, //class ID (5 digit)
   bookingID: String,
@@ -74,24 +104,19 @@ const TeacherSchema = new Schema({
     type: String,
     required: false,
   },
+  profileImage:{
+    type:Buffer,
+    required:false
+  },
   totalEarnings:{   //account earning +
     type:Number,
     required: false,
   },
   bookings: [ ],
   schedule: [],
+  education:[],
+  work:[],
   account: [],
 });
 
 module.exports = Teacher = mongoose.model("teacher", TeacherSchema);
-
-// bookingid: [
-//   {
-//     username: String,
-//     subject: String,
-//     date: String,
-//     status: Boolean,
-//     time: String,
-//     amount: String,
-//   },
-// ],
